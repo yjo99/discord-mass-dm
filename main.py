948bd3e0a61c71344868551d8f6cf55c79a9315a
Yousef_Sayed <yousef16992@gmail.com>
2023-03-10 16:25:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import logging
 import asyncio
 from tasksio import TaskPool
 from datetime import datetime
-from pycuolor import color
+from pycoloring import color
 from lib.scraper import Scraper
 from aiohttp import ClientSession
 
